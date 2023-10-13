383856f0a3e1c785d9aa37a1fb4602eb02d87eeb
signalguy <sacheol.yang@gmail.com>
2023-10-13 20:56:44 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import csv
+import pandas
+
+data = []
+csv_data = []
+temperatures = []
+temp = 0
+
+file = open("weather_data.csv")
+# data.append(file.readlines())
+csv_data = csv.DictReader(file)
+print(csv_data)
+for row in csv_data:
+    print(row)
+    temperatures.append(int(row.get("temp")))
+file.close()
+
+print(temperatures)
+
+data = pandas.read_csv("weather_data.csv")
+data_list = data["temp"].to_list()
+
+idx = 0
+sum = 0
+for temp in data_list:
+    sum += temp
+    idx += 1
+
+avg = sum / idx
+print(avg)
+# print(data_list)
+
